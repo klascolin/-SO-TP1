0528740eaac6885a5d928f8180a0722580a2be6c
Nicolas Mastropasqua <nmastropasqua@ws16.labo4>
2015-09-24 20:19:59 -0300

    Ultimo del ej3

--- tasks.cpp
+++ tasks.cpp
@@ -27,28 +27,55 @@ void TaskBatch(int pid, vector<int> params){
 	//Nuevo:
 	
 	vector<int> lista;
-	for(int i = 0; i < params[0];i++){
+	for(int i = 0; i < params[0]-1;i++){
 		lista.push_back(i);
 	}
 	std::random_shuffle(lista.begin(),lista.end());
 	int listaArray[params[0]-1];
-	for(int i=0;i<params[1];i++){
-		listaArray[lista[i]] = 1;
+	for(i =0;i<lista.size();i++){
+		listaArray[lista[i]] == 1;
 	}
-	for(int i=0;i<params[0];i++){		
+	for(i=0;i<params[0]-1;i++){		
 		if(listaArray[i] == 1){
 			uso_IO(pid,1);
 		}else{
 			uso_CPU(pid,1);
 		}
 	}
-	/*
-	 * TaskBatch 20 2
-TaskBatch 60 9
-TaskBatch 22 5
-
+	
+	
+	//Viejo:
+	//~ int lista [params[0]-1];
+	//~ for(int j =0;j<params[0]-1;j++){
+		//~ lista[j] = 0;
+	//~ }
+	//~ int cont = 0;
+	//~ int i = 0;
+	//~ int rnd;
+	//~ while(cont < params[1]){
+		//~ rnd = (rand() % 60);	
+		//~ //cout << rnd << endl;
+		//~ if(rnd <=29 && lista[i] == 0){
+			//~ lista[i] = 1;
+			//~ cont ++;
+		//~ }
+		//~ i++;
+		//~ if(i >= params[0]){
+			//~ i==0;
+		//~ }
+	//~ }
+	//~ for(i=0;i<params[0]-1;i++){		
+		//~ if(lista[i] == 1){
+			//~ uso_IO(pid,1);
+		//~ }else{
+			//~ uso_CPU(pid,1);
+		//~ }
+	//~ }
+	/*Hago una explicacion!! El primer while es el que decide cuales son los bloqueos, y el for, es el que dice ahora ejecuta IO, ahora CPU..
 	 * 
-	 * */
+	 * Entonces lo raro es que, si vos pones los couts, la lista ya no es lo que era.. osea, yo asigno en la posicion de la lista, 7 y 8 
+	 * un 1, y despues la imprimis y tiene todos ceros..*/
+	
 
 }
 //Esta tarea genera n llamadas bloqueantes con duracion al azar(entre bmin y bmax)
@@ -56,11 +83,12 @@ void TaskConsola(int pid, vector<int> params){ //params: n,bmin,bmax
 	//Obtenemos una semilla a partir de la hora
 	//srand(time(NULL));
 
-	int range =  params[2] - params[1] +1;
+	int range =  params[1] - params[2];
+	int rmax = RAND_MAX;
 	int i,rnd;
 	
 	for(i=0;i<params[0];i++){
-		rnd = (rand() % range ) + params[1];	
+		rnd = (rand() / (rmax / range)) + params[2];	
 		uso_IO(pid, rnd); // Uso IO rnd ms
 	}
 
